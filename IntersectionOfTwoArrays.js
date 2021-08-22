2805d64447fc817eb2baea550aaed754b548fc2a
Vik <vvang044@gmail.com>
2021-08-21 23:11:55 -0400

    Easy Intersection of Two arrays

---
+++ IntersectionOfTwoArrays.js
@@ -0,0 +1,29 @@
+/*
+Intersection of Two Arrays
+Given two integer arrays nums1 and nums2, return an array of their intersection. 
+Each element in the result must be unique and you may return the result in any order.
+
+Example 1:
+Input: nums1 = [1,2,2,1], nums2 = [2,2]
+Output: [2]
+
+Example 2:
+Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+Output: [9,4]
+Explanation: [4,9] is also accepted.
+
+*/
+
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+ var intersection = function(nums1, nums2) {
+    
+    let result = nums1.filter(value => nums2.includes(value))
+    
+          .filter((value, index, arr) => arr.indexOf(value) === index);
+      
+      return result
+  };
