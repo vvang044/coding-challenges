c4d3a555591381d94b6a66f7d8a43f9e67a2f344
Vik <vvang044@gmail.com>
2021-09-01 18:41:10 -0400

    Add zero's if you find zero and keep array length same

---
+++ DuplicateZeros.js
@@ -0,0 +1,34 @@
+/*
+Given a fixed-length integer array arr, duplicate each occurrence of zero,
+ shifting the remaining elements to the right.
+
+Note that elements beyond the length of the original array are not written. 
+Do the above modifications to the input array in place and do not return anything.
+
+Example 1:
+Input: arr = [1,0,2,3,0,4,5,0]
+Output: [1,0,0,2,3,0,0,4]
+Explanation: After calling your function,
+ the input array is modified to: [1,0,0,2,3,0,0,4]
+
+ Example 2
+ Input: arr = [1,2,3]
+Output: [1,2,3]
+Explanation: After calling your function, the input array is modified to: [1,2,3]
+*/
+
+var duplicateZeros = function(arr){
+    for (var i = 0; i < arr.length; ++i) {
+        if (arr[i] == 0) {
+            // arr.splice(arr.indexOf(0, i), 0, 0)
+            //splice(start, deleteCount, item1)
+            arr.splice(i,0,0)
+            arr.pop()
+            i ++
+        }
+    }
+    return arr //not required but just to test it [1,0,0,1]
+
+}
+
+console.log(duplicateZeros([1,0,2,3]))
