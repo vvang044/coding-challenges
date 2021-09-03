35a5e0a3878d1e056d4316f397602b446584c460
Vik <vvang044@gmail.com>
2021-09-03 00:40:26 -0400

    Easy - Add String - LeetCode

--- AddString.js
+++ AddString.js
@@ -16,10 +16,31 @@ You must solve the problem without using any built-in library for
  * @return {string}
  */
  var addStrings = function(num1, num2) {
-    let numOneInt = parseInt(num1)
-    let numTwoInt = parseInt(num2)
+    // let numOneInt = parseInt(num1)
+    // let numTwoInt = parseInt(num2)
     
-    let result = numOneInt + numTwoInt
+    // let result = numOneInt + numTwoInt
     
-    return (JSON.stringify(result))
+    // return (JSON.stringify(result))
+
+
+    sum = '';
+    len1 = num1.length;
+    len2 = num2.length;
+    carry = 0;
+
+    while(len1 > 0 || len2 > 0 || carry) {
+        num1Digit = +num1.charAt(len1-1);
+        num2Digit = +num2.charAt(len2-1);
+        len1--;
+        len2--;
+        
+        digitTotal = num1Digit + num2Digit + carry;
+        carry = Math.floor(digitTotal / 10);
+        digitToAppend = digitTotal % 10;
+        sum = digitToAppend + sum;
+    } 
+
+    return sum;
+
 };
